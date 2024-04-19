b3ee08d12b9d403c69809d26cdc18887b5f0227f
Lucasz24 <Lucasviegasdefreitas07@gmail.com>
2024-04-18 21:56:51 -0300

    Criada tela de conta

--- WinFormsApp1Algoritimo/Form1.cs
+++ WinFormsApp1Algoritimo/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp1Algoritimo
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
